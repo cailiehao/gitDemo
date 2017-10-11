9bae289fa273a08c734e0d7fb2f23693e38f5f32
cailiehao <hanyuy@163.com>
2017-10-11 15:46:10 +0800

    初始化创建内容

---
+++ src/main/java/com/test/hc/gitDemo/App.java
@@ -0,0 +1,12 @@
+package com.test.hc.gitDemo;
+
+/**
+ * Demo
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Git Demo" );
+    }
+}
