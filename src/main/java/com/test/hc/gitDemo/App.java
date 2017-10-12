b8c63c4a1a6ab7eedf2b1d2b37eaee8c8e30d2ff
cailiehao <hanyuy@163.com>
2017-10-12 14:38:30 +0800

    地方的方法

--- src/main/java/com/test/hc/gitDemo/App.java
+++ src/main/java/com/test/hc/gitDemo/App.java
@@ -10,5 +10,6 @@ public class App
         System.out.println( "Hello World! Git Demo" );
         System.out.println("添加第二次的修改内容，比对");
         System.out.println("能不能记住密码");
+        System.out.println("444444444444444444444444");
     }
 }
